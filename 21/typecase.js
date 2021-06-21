96e2f5a6644709928ce7a37c9c588f0b35227f18
VARSHAC11 <85901564+VARSHAC11@users.noreply.github.com>
2021-06-21 22:27:59 +0530

    Create typecase.js

---
+++ 21/typecase.js
@@ -0,0 +1,27 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+(function (str)
+{
+  str = str.toLowerCase().split(' ');
+  for (var i = 0; i < str.length; i++) 
+  {
+    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+  }
+  console.log( str.join(' '));
+  //end-here
+})("varsha");
+});
