5967b62d5686ca4bbbd56ffeef31b1715a1219cc
VARSHAC11 <85901564+VARSHAC11@users.noreply.github.com>
2021-06-22 09:31:35 +0530

    Create odd_no.js

---
+++ 21/arrow/odd_no.js
@@ -0,0 +1,38 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+b=[]
+c=[]
+var t=(...input)=> 
+{
+    for(i=0;i<input.length;i++)
+    {
+        if(input[i]%2==0)
+        {
+            b.push(input[i]);
+        }
+        else
+        {
+            c.push(input[i]);
+        }
+    }
+    console.log(c);
+}
+ 
+t(1,2,3,4,5)
+
+  //end-here
+});
